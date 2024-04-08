b635932e9bf1e1afb16e1e68b94f065e36e8dfee
annahico <annahico@gmail.com>
2024-04-08 20:54:07 +0200

    realizando retos vanilla JS II

--- js/main.js
+++ js/main.js
@@ -4,26 +4,46 @@
 //     números. Este método devolverá un número entero aleatorio. Muestra estos números por
 //     consola.
 
-function generarNumeroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+// function generarNumeroAleatorio(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// function generarNumeros(cantidad, min, max) {
+//     let numeros = [];
+//     for (let i = 0; i < cantidad; i++) {
+//         let numeroAleatorio = generarNumeroAleatorio(min, max);
+//         numeros.push(numeroAleatorio);
+//     }
+//     return numeros;
+// }
+
+// function main() {
+//     let cantidadNumeros = parseInt(prompt("Ingrese la cantidad de números a generar:"));
+//     let minimo = parseInt(prompt("Ingrese el valor mínimo:"));
+//     let maximo = parseInt(prompt("Ingrese el valor máximo:"));
+
+//     let numerosGenerados = generarNumeros(cantidadNumeros, minimo, maximo);
+//     console.log("Números generados:", numerosGenerados);
+// }
+
+// main();
+
+//----------------------O---------------------------------//
+const prompt = require("prompt-sync")();
+
+const generateRandomInteger = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+let numberOfIntegers = prompt("Diga la cantidad de números: ");
+let min = parseInt(prompt("Valor mínimo: "));
+let max = parseInt(prompt("Valor máximo: "));
+const numbers = [];
+
+for (let i = 0; i < numberOfIntegers; i++) {
+    let number = generateRandomInteger(min, max);
+    numbers[i] = number;
 }
 
-function generarNumeros(cantidad, min, max) {
-    let numeros = [];
-    for (let i = 0; i < cantidad; i++) {
-        let numeroAleatorio = generarNumeroAleatorio(min, max);
-        numeros.push(numeroAleatorio);
-    }
-    return numeros;
+for (const n of numbers) {
+    console.log(n);
 }
-
-function main() {
-    let cantidadNumeros = parseInt(prompt("Ingrese la cantidad de números a generar:"));
-    let minimo = parseInt(prompt("Ingrese el valor mínimo:"));
-    let maximo = parseInt(prompt("Ingrese el valor máximo:"));
-
-    let numerosGenerados = generarNumeros(cantidadNumeros, minimo, maximo);
-    console.log("Números generados:", numerosGenerados);
-}
-
-main();
